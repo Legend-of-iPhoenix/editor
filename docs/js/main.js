3ce6bb4d2cfef908176d1e28d59ada48564b9fc0
LAX18 <34464552+LAX18@users.noreply.github.com>
2018-06-14 11:39:56 -0400

    Add Subscript and Superscript (#11)
    * Delete style.css
    
    * Delete main.js
    
    * Delete index.html
    
    * Add files via upload
    
    * Rename style.css to docs/css/style.css
    
    * Rename main.js to doc/js/main.js
    
    * Rename index.html to docs/index.html
    
    * Change main.js (part 1 of 2)
    
    Add function to handle (custom) keyboard shortcuts, generalize the formatting commands.
    
    * Change how JS works. (part 2 of 2)
    
    * Move something, add gitignore
    
    doc/js/main.js => docs/js/main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,12 +1,30 @@
-
-function bold() {
-  document.execCommand("bold", false);
+function format(style) {
+  document.execCommand(style, false);
 }
 
-function italics() {
-  document.execCommand("italic", false);
+function heading() {
+  document.execCommand("heading", false, '<h1>')
 }
 
-function underline() {
-  document.execCommand("underline", false);
+window.onkeydown = function(event) {
+  if (event.metaKey) { // i.e. command key on mac
+    if (event.key == 'b') {
+      format('bold');
+    }
+    if (event.key == 'i') {
+      format('italics');
+    }
+    if (event.key == 'u') {
+      format('underline');
+    }
+    if (event.key == 's' && event.shiftKey) { // ctrl-shift-s
+      format('strikethrough');
+    }
+    if (event.key == 'ArrowUp' && event.shiftKey) { // ctrl-shift-up
+      format('superscript');
+    }
+    if (event.key == 'ArrowDown' && event.shiftKey) { // you get the idea
+      format('subscript');
+    }
+  }
 }
