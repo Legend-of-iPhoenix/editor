92c69493bed256d541a5e83e4484f7624f916bcb
jcgter777 :P <34496783+jcgter777@users.noreply.github.com>
2018-06-14 16:36:48 -0400

    Add stuff traitors did

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,15 +1,40 @@
-
-
-function bold() {
-  document.execCommand("bold", false);
+function format(style) {
+  document.execCommand(style, false);
 }
 
-function italics() {
-  document.execCommand("italic", false);
+function headingOne() {
+  document.execCommand("heading", false, '<h1>')
 }
 
-function underline() {
-  document.execCommand("underline", false);
+function headingTwo() {
+  document.execCommand("heading", false, '<h2>')
+}
+
+function headingThree() {
+  document.execCommand("heading", false, '<h3>')
+}
+
+window.onkeydown = function(event) {
+  if (event.metaKey) { // i.e. command key on mac
+    if (event.key == 'b') {
+      format('bold');
+    }
+    if (event.key == 'i') {
+      format('italics');
+    }
+    if (event.key == 'u') {
+      format('underline');
+    }
+    if (event.key == 's' && event.shiftKey) { // ctrl-shift-s
+      format('strikethrough');
+    }
+    if (event.key == 'ArrowUp' && event.shiftKey) { // ctrl-shift-up
+      format('superscript');
+    }
+    if (event.key == 'ArrowDown' && event.shiftKey) { // you get the idea
+      format('subscript');
+    }
+  }
 }
 
 function toggleformat() {
